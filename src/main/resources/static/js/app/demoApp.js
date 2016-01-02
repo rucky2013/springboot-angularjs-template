4056f98960260e3e1016d76a7af62e352fafd246
gung <rryabec@gmail.com>
2016-01-02 23:15:43 +0200

    Crud works

--- src/main/resources/static/js/app/demoApp.js
+++ src/main/resources/static/js/app/demoApp.js
@@ -2,9 +2,39 @@
  * This is simple Spring Boot + Angular Demo application 
  */
 
-var demoApp = angular.module('demoApp',['ngRoute']);
+var demoApp = angular.module('demoApp',['ngRoute','CrudService']);
 
-demoApp.controller('CrudCtrl',['$scope',function($scope){
+demoApp.controller('CrudCtrl',['$scope','Person',function($scope,Person){
+	$scope.person = {fistName:undefined,lastName:undefined};
+	
+	$scope.refresh = function(){
+		$scope.people = Person.query();
+	};
+	
+	$scope.refresh();
+	
+	$scope.save = function(){
+		Person.save($scope.person
+				, function(){
+					$scope.person.firstName = undefined;
+					$scope.person.lastName = undefined;
+					$scope.refresh();
+					console.log("Person saved");
+				}
+				, function(){console.log("Failed save person")});
+	};
+	
+	$scope.delete = function(person){
+		console.log(person._links.self.href);
+		Person.delete(person._links.self.href,
+				function(){
+			console.log("Success ...");
+		},
+		function(){
+			console.log("Failed delete person");
+		});
+		console.log(person._links.self.href);
+	};
 	
 }]);
 
