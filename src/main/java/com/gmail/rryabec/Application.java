237a4027d6dfce66457a798b270df9fab125dfc4
rryabec <rryabec@gmail.com>
2016-01-09 10:26:35 +0200

    Move war configuration from master to war branch

--- src/main/java/com/gmail/rryabec/Application.java
+++ src/main/java/com/gmail/rryabec/Application.java
@@ -8,15 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class Application extends SpringBootServletInitializer{
-	
-	
-	
-    @Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(Application.class);
-	}
-
+public class Application{
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
