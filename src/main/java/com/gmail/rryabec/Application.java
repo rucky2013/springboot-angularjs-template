ab7c5fe1c0769abe0e81434c43e3fc791c6a8802
rryabec <rryabec@gmail.com>
2016-01-09 10:29:20 +0200

    Add war deploy

--- src/main/java/com/gmail/rryabec/Application.java
+++ src/main/java/com/gmail/rryabec/Application.java
@@ -2,12 +2,20 @@ package com.gmail.rryabec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class Application {
+public class Application  extends SpringBootServletInitializer{
+	
 
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
